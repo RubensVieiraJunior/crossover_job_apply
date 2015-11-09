b2eb7b5d9c24e8ff8f25ab2199d4a02197132ec0
Rubens Luiz Vieira Junior <rubens.vieira.junior@gmail.com>
2015-11-09 06:11:31 -0200

    Webservice

--- webservice/src/main/java/com/dev/webservice/BackEnd.java
+++ webservice/src/main/java/com/dev/webservice/BackEnd.java
@@ -3,11 +3,15 @@
  */
 package com.dev.webservice;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.dev.persistence.CustomerDAO;
-import com.dev.persistence.CustomerImplDAO;
+import com.dev.persistence.dao.CustomerDAO;
+import com.dev.persistence.dao.CustomerImplDAO;
 
 /**
  * @author Rubens Luiz Vieira Junior
@@ -17,10 +21,13 @@ public class BackEnd {
 
 	Logger logger = LoggerFactory.getLogger(BackEnd.class);
 	
-	public String save(String jsonObject) {
+	public String save(int objectType, String jsonObject) {
 		String METHOD = "save(String jsonObject) ";
 		
 		logger.debug(METHOD + "Param:" + jsonObject);
+		
+		JSONObject jsObject = new JSONObject();
+		
 		
 		CustomerDAO customerDao = new CustomerImplDAO();
 		customerDao.beginTransaction();
